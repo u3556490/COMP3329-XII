4a9048809fedc83014fee7de54d4d4b513884afd
Justin <u3556490@hku.hk>
2020-03-08 20:07:03 +0800

    fixed animation and flipping.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,14 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D playerRigidbody;
     private Vector3 changepos;
+    private bool facingRight = false;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,13 +23,53 @@ public class PlayerMovement : MonoBehaviour
         changepos = Vector3.zero;
         changepos.x = Input.GetAxisRaw("Horizontal");
         changepos.y = Input.GetAxisRaw("Vertical");
-        if (changepos!=Vector3.zero) MoveChar();
+        if (Input.GetAxisRaw("Horizontal") > 0.5f && !facingRight)
+        {
+            //If we're moving right but not facing right, flip the sprite and set     facingRight to true.
+            Flip();
+            facingRight = true;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0.5f && facingRight)
+        {
+            //If we're moving left but not facing left, flip the sprite and set facingRight to false.
+            Flip();
+            facingRight = false;
+        }
+        UpdateAnimation();
+        
+
     }
 
-    void MoveChar()
+    private void UpdateAnimation()
+    {
+        if (changepos != Vector3.zero)
+        {
+            MoveChar();
+            animator.SetFloat("moveX", changepos.x);
+            animator.SetFloat("moveY", changepos.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    private void MoveChar()
     {
         playerRigidbody.MovePosition(
                 transform.position + changepos*speed*Time.deltaTime
             );
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
---
+++ justin_readme.txt
@@ -0,0 +1,2 @@
+following this tutorial ^^
+https://www.youtube.com/watch?v=Vfq13LRggwk
