b83b1fefe9504bfbf65c800dad1ff738c9a92de4
Justin <u3556490@hku.hk>
2020-03-18 18:37:05 +0800

    added smooth camera animation

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing;
+    public Vector2 max_xy;
+    public Vector2 min_xy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // LateUpdate is called the last
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+            targetPos.x = Mathf.Clamp(targetPos.x, min_xy.x, max_xy.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, min_xy.y, max_xy.y);
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+        }
+    }
+}
