13de7b4b2cbe91616da344da89968ca2a004adc8
Justin <u3556490@hku.hk>
2020-04-01 19:40:38 +0800

    Starting to make a rhythm game part

---
+++ Assets/Scripts/MG_NoteObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_NoteObject : MonoBehaviour
+{
+
+    public bool pressable = false;
+    public KeyCode keyToPress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress) && pressable)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            pressable = true;
+        }
+    }
+
+    void onTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            pressable = false;
+        }
+    }
+}
