fff4b26e7b130c742f5e516391e1367c4db84c53
Justin <u3556490@hku.hk>
2020-04-28 11:29:20 +0800

    added transition from house to overworld stage 1 and vice versa

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+
+    public string sceneToLoad;
+    public Vector2 playerPos;
+    public VectorValue playerSave;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerSave.initialValue = playerPos;
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScriptableObject/MakeScriptableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MakeScriptableObject
+{
+    [MenuItem("Assets/Create/VectorValue")]
+    public static void CreateMyAsset()
+    {
+        VectorValue asset = ScriptableObject.CreateInstance<VectorValue>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/Scripts/ScriptableObject/NewScripableObject.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
