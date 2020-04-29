28ff6b730745ff9476ea0a037999c1fffda669dc
Justin <u3556490@hku.hk>
2020-04-29 12:39:45 +0800

    added click SFX and BGM for stage 1

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    //use GameObject.FindGameObjectWithTag("Music").GetComponent<BGM>().PlayMusic(); to start playing
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
